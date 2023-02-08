dac03cfb0cbe6c17b8f7e765781812451bb0503c
TheQuintillo <thequintillo@gmail.com>
2023-02-08 22:17:37 +0100

    chore: probando mensaje

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function index() {
+    return (
+        <>
+     <div>index</div>
+    <div>
+        <li>
+                    <ul></ul>
+                    <ul></ul>
+                    <ul></ul>
+                    <ul></ul>
+                    <ul></ul>
+                </li>
+            </div>
+        </>
+    );
+}
+
+export default index;
